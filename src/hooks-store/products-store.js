53e375026677a059e75c5c7b719164bca97542e0
Magno Sescon <magnosescon@gmail.com>
2020-11-08 00:05:39 -0300

    refactor: Using the Custom Store

--- src/hooks-store/products-store.js
+++ src/hooks-store/products-store.js
@@ -4,7 +4,7 @@ const configureStore = () => {
   const actions = {
     TOGGLE_FAV: (curState, productId) => {
       const prodIndex = curState.products.findIndex(p => p.id === productId);
-      const newFavStatus = !currentProdList[prodIndex].isFavorite;
+      const newFavStatus = !curState.products[prodIndex].isFavorite;
       const updatedProducts = [...curState.products];
       updatedProducts[prodIndex] = {
         ...curState.products[prodIndex],
@@ -13,32 +13,34 @@ const configureStore = () => {
       return {products: updatedProducts};
     }
   };
-  initStore(actions, {products:[
-    {
-      id: 'p1',
-      title: 'Red Scarf',
-      description: 'A pretty red scarf.',
-      isFavorite: false
-    },
-    {
-      id: 'p2',
-      title: 'Blue T-Shirt',
-      description: 'A pretty blue t-shirt.',
-      isFavorite: false
-    },
-    {
-      id: 'p3',
-      title: 'Green Trousers',
-      description: 'A pair of lightly green trousers.',
-      isFavorite: false
-    },
-    {
-      id: 'p4',
-      title: 'Orange Hat',
-      description: 'Street style! An orange hat.',
-      isFavorite: false
-    }
-  ]});
+  initStore(actions, {
+    products:[
+      {
+        id: 'p1',
+        title: 'Red Scarf',
+        description: 'A pretty red scarf.',
+        isFavorite: false
+      },
+      {
+        id: 'p2',
+        title: 'Blue T-Shirt',
+        description: 'A pretty blue t-shirt.',
+        isFavorite: false
+      },
+      {
+        id: 'p3',
+        title: 'Green Trousers',
+        description: 'A pair of lightly green trousers.',
+        isFavorite: false
+      },
+      {
+        id: 'p4',
+        title: 'Orange Hat',
+        description: 'Street style! An orange hat.',
+        isFavorite: false
+      }
+    ]
+  });
 }
 
 export default configureStore;
